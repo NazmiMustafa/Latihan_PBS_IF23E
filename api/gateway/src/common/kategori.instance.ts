3c8a02d9d3562cc5ae35768a3413c5a2c6d2e8b5
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-04-29 11:24:25 +0700

    membikin gateway

---
+++ api/gateway/src/common/kategori.instance.ts
@@ -0,0 +1,25 @@
+import { HttpException } from '@nestjs/common';
+import axios, { AxiosError } from 'axios';
+
+// buat variabel untuk endpoint kategori
+export const kategori_api = axios.create({
+  baseURL: 'http://localhost:3001/api/kategori',
+  timeout: 1000,
+});
+
+// buat interceptor untuk response
+kategori_api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    const message = error.response?.data;
+    const status = error.response?.status;
+
+    // jika terbaca status error
+    if (status && message) {
+      throw new HttpException(message, status);
+    }
+
+    // jika tidak terbaca status error
+    throw new HttpException('Kategori Error', 500);
+  },
+);
---
+++ api/gateway/src/kategori/kategori.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { kategori_api } from '../common/kategori.instance';
+import { CreateKategoriDto } from './dto/create-kategori.dto';
+import { UpdateKategoriDto } from './dto/update-kategori.dto';
+
+// buat interface untuk data kategori
+export interface Kategori {
+  id: number;
+  nama: string;
+  nama_filter: string;
+}
+@Injectable()
+export class KategoriService {
+  private readonly base_url = 'http://localhost:3001/api/kategori';
+  // buat variabel untuk endpoint kategori
+
+  // fungsi untuk akses
+  // endpoint kategori (create)
+  async create(createKategoriDto: CreateKategoriDto): Promise<Kategori> {
+    // return 'This action adds a new kategori';
+    const response = await kategori_api.post<Kategori>('/', createKategoriDto);
+    return response.data;
+  }
+
+  // fungsi untuk akses
+  // endpoint kategori (findAll)
+  async findAll(): Promise<Kategori[]> {
+    // return `This action returns all kategori`;
+    const response = await kategori_api.get<Kategori[]>('/');
+    return response.data;
+  }
+
+  // fungsi untuk akses
+  // endpoint kategori (findOne)
+  async findOne(id: number): Promise<Kategori> {
+    // return `This action returns a #${id} kategori`;
+    // const response = await axios.get<Kategori>(`${this.base_url}/${id}`);
+    // return response.data;
+    const response = await kategori_api.get<Kategori>(`/${id}`);
+    return response.data;
+  }
+
+  async update(
+    id: number,
+    updateKategoriDto: UpdateKategoriDto,
+  ): Promise<Kategori> {
+    // return `This action updates a #${id} kategori`;
+    const response = await kategori_api.patch<Kategori>(
+      `/${id}`,
+      updateKategoriDto,
+    );
+    return response.data;
+  }
+
+  async remove(id: number): Promise<Kategori> {
+    // return `This action removes a #${id} kategori`;
+    const response = await kategori_api.delete<Kategori>(`/${id}`);
+    return response.data;
+  }
+}
