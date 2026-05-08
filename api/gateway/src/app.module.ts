563223b527d0c628e0c7cb337c6fa483b42b33bd
NazmiMustafa <nazmi_mustafa@teknokrat.ac.id>
2026-05-08 17:11:51 +0700

    instal jwt

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KategoriModule } from './kategori/kategori.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [KategoriModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [KategoriModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
